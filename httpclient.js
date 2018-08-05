463cc189b2d5def1fa27dd85bddc6c42e672f950
Damion Gibbs <dmgibbs@writeme.com>
2018-08-05 01:53:37 +0000

    Updated http info. file up to step 5

--- httpclient.js
+++ httpclient.js
@@ -1,3 +1,21 @@
 var https = require('https');
 
-console.log('I did it!');
+var options = {
+  host: 'www.google.com',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function( response) {
+  console.log('In response handler callback!');
+  console.log('Response ', response);
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+
+console.log('End of program.');
